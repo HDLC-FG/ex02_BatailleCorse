1c3d4b2c465aa2cb085f9dca25da2b6b00393d1e
HDLC <henri.libault-de-la-chevasnerie@fortil.group>
2024-12-02 14:48:10 +0100

    new solution

---
+++ ex02_BatailleCorse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex02_BatailleCorse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
